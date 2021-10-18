321561af8a04ae3980e9276a15bf847c8629f532
rafalosa <rafal.osad@gmail.com>
2021-10-18 20:28:59 +0200

    Chamber computer program skeleton.

--- chamber/chamber.py
+++ chamber/chamber.py
@@ -3,65 +3,115 @@ import socket
 import time
 import yaml
 import smbus
+from threading import Thread
+from queue import Queue
 
-HEADER_SIZE = 10
 
-with open("config/chamber_config.yaml", "r") as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
+# todo: Wrap this in an object.
 
-main_i2c_bus = smbus.SMBus(1)
+def measurement_dummy(q):
+    num = 30
+    sum_ = 0
 
-address = config["IP"]
-port = config["PORT"]
+    for i in range(num):
+        sum_ += sensors.ADS1115ADC.fake_read()
+        time.sleep(0.05)
 
-running = True
+    q.put(sum_/num)
 
-grav_sensor = sensors.LIS3DHAccelerometer(config["GRAV"], main_i2c_bus)
-grav_sensor.enable()
-index = 0
-means = [0,0,0]
 
-while running:
+if __name__ == "__main__":
 
-    # Once per 20min take a picture of chamber inside with flash.
-    # One per 10 min make a humidity measurement.
-    # message scheme:
-    # grav_x;grav_y;grav_z;temp1;temp2;temp3;light1;light2;humidity;time since last humidity test
+    HEADER_SIZE = 10
 
-    # formulate message, ';' delimiter
-    accel_vals = grav_sensor.read()
-    accel_vals = [val/(2**16/4) for val in accel_vals]
-    temp = [means[ind] * index / (index + 1) + accel_vals[ind] / (index + 1) for ind in range(3)]
-    index += 1
-    means = temp
-    grav_values = accel_vals + means
-    msg = ";".join([str(val) for val in grav_values]) + "\n"
-    msg = f'{len(msg):<{HEADER_SIZE}}' + msg
-    print(msg)
+    with open("config/chamber_config.yaml", "r") as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
-        sc.settimeout(10)
-        try:
-            sc.connect((address, port))
-        except socket.timeout:
-            print("Connection timed out.")
-            running = False
-            break
-        except ConnectionRefusedError:
-            break
-        sc.sendall(msg.encode())
-        response = ""
+    address = config["IP"]
+    port = config["PORT"]
+
+    running = True
+    saturation_queue = Queue()
+
+    main_i2c_bus = smbus.SMBus(1)
+    grav_sensor = sensors.LIS3DHAccelerometer(config["GRAV"], main_i2c_bus)
+    grav_sensor.enable()
+
+    index = 0
+    means = [0, 0, 0]
+    last_measurement_timestamp = time.time()
+    current_timestamp = time.time()
+
+    while True:
+
         while True:
-            data = sc.recv(HEADER_SIZE)
-            data = data.decode('utf-8')
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
 
-            if fresh:
-                size = int(data[:HEADER_SIZE])
-                fresh = False
-                response += data[HEADER_SIZE:]
-            else:
-                response += data
+                sc.settimeout(10)
+                try:
+                    # print(f"Attempting connection to {address}")
+                    sc.connect((address, port))
+                except socket.timeout:
+                    print("Connection timed out.")
+                    break
+                except ConnectionRefusedError:
+                    index = 0
+                    means = [0, 0, 0]
+                    saturation_measurement_scheduled = False
+                    print("Connection refused, reconnection attempt in 5s.")
+                    time.sleep(5)
+                    break  # Here interrupt the forever loop and continue to watch fot the connection.
 
-            if len(response) == size:
-                break
-    time.sleep(0.2)
+                current_timestamp = time.time()
+                if current_timestamp - last_measurement_timestamp >= 0.5 * 60:
+                    print("Scheduling saturation measurement.")
+                    Thread(target=measurement_dummy, args=(saturation_queue,))
+                    last_measurement_timestamp = current_timestamp
+
+                accel_values = grav_sensor.read()
+                accel_values = [val / (2 ** 16 / 4) for val in accel_values]
+                temp = [means[ind] * index / (index + 1) + accel_values[ind] / (index + 1) for ind in range(3)]
+                index += 1
+                means = temp
+                sensor_values = accel_values + means
+                temperatures = [sensors.MCP9808Thermometer.fake_read() for _ in range(3)]
+                sensor_values += temperatures
+
+                if not saturation_queue.empty():
+                    val = saturation_queue.get()
+                    sensor_values += [val]
+                    saturation_queue.task_done()
+
+                msg = ";".join([str(val) for val in sensor_values]) + "\n"
+                msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+
+                fresh = True
+                try:
+                    sc.sendall(msg.encode())
+                except BrokenPipeError:
+                    index = 0
+                    means = [0, 0, 0]
+                    saturation_measurement_scheduled = False
+                    break
+
+                response = ""
+
+                while True:
+                    data = sc.recv(HEADER_SIZE)
+                    data = data.decode('utf-8')
+
+                    if fresh:
+                        size = int(data[:HEADER_SIZE])
+                        fresh = False
+                        response += data[HEADER_SIZE:]
+                    else:
+                        response += data
+
+                    if len(response) == size:
+                        break
+                response_components = response.split(";")
+                if response_components[0] != "default":
+                    # Adjust lighting
+                    print("adjusting lighting", int(response_components[0]), int(response_components[1]))
+
+                time.sleep(0.2)
